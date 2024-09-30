6f931f8208fc44aeb678199bdd5404330b684368
Esther <ifacesther@gmail.com>
2024-09-30 15:32:23 -0500

    Atividade Concluida - Esther

---
+++ Atividade 01/src/SomaNumeros.java
@@ -0,0 +1,50 @@
+public class SomaNumeros {
+    public static void main(String[] args) {
+        
+        int num1 = 10;
+        int num2 = 25;
+        
+        //Realize e exiba a soma
+        int soma = num1 + num2;  // Realizando a soma
+        System.out.println("A soma de " + num1 + " e " + num2 + " é: " + soma);
+        
+        //Realize e exiba a multiplicação
+        int multiplica = num1 * num2;  // Realizando a multiplicação
+        System.out.println("A multiplicação de " + num1 + " e " + num2 + " é: " + multiplica);
+        
+        //Realize e exiba a divisão
+        int divisao = num1 / num2;
+        System.out.println("A divisão de " + num1 + " e " + num2 + " é: " + divisao);
+
+        //Realize e exiba o resto da divisão
+        int restoDivisao = num1 % num2;
+        System.out.println("o resto da divisao de " + num1 + " e " + num2 + " é: " + restoDivisao);
+        
+        //Realize e exiba a soma de dois números ponto flutuante do tipo FLOAT (Obs: pode utilizar dois números fixos)
+        float numero1 = 10.5f;
+        float numero2 = 2.0f;
+
+        //Realize e exiba a soma
+        float somaFloat = numero1 + numero2;
+        System.out.println("A soma do numero de ponto flutuante float " + num1 + " e " + num2 + " é: " + somaFloat);
+        //Realize e exiba a subtração
+        
+        float subtraiFloat = numero1 - numero2;
+        System.out.println("A subtração do numero de ponto flutuante float  " + num1 + " e " + num2 + " é: " + subtraiFloat);
+        
+        //Realize e exiba a divisão de dois números ponto flutuante do tipo DOUBLE (Obs: pode utilizar dois números fixos)
+        Double n1 = 25.0;
+        Double n2 = 5.0;
+        
+        //Realize e exiba a divisão
+        Double divideDouble = n1 / n2;
+        System.out.println("A divisão do numero de ponto flutuante double  " + num1 + " e " + num2 + " é: " + divideDouble);
+        
+        //Realize e exiba a multiplicação
+        Double multilicaDouble = n1 * n2;
+        System.out.println("A multiplicação do numero de ponto flutuante double  " + num1 + " e " + num2 + " é: " + multilicaDouble);
+        
+        
+    }
+}
+
